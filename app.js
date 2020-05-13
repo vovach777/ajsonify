9f26ba08aefac533514a80daa3d1f9f359438e06
vovach777 <vovach777@gmail.com>
2020-05-13 15:48:48 +0300

    v2.0.0

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-const { createJSONStream } = require('./index');
+const { createJSONStream, copy_object, stringify_g } = require('./index');
 const { createWriteStream } = require('fs');
 
 let o = {
    hello: 'world',
    num:  1,
    child : { arr: [1,2,3,4,'string'] },
+   array: [null,void 0, void 0,null],
    no : null,
+   undefined: void 0,
    rus: 'русские буквы',
    rus2: '\\слеш \\ слеш \\ слеш \\ бэкслеш / бэкслеш /',
    kav1: `'`,
@@ -14,21 +16,40 @@ let o = {
    date: new Date(),
    string: new String('string'),
    number: new Number(123456),
-   boolean: new Boolean(false)
+   boolean: new Boolean(false),
+   symbol:  Symbol('symbol'),
+   empty_array: [],
+   empty_object: {}
 }
+let o_cp = {...o};
 //circular ref. protection test
 o.o = o;
-o.child.arr.push({...o});
+o[ o.special ] = 'special';
+o.child2 = o.child;
+o.empty_4 = [];
+o.empty_4.length = 4;
+
+
+console.dir( o , {depth: null});
+console.dir( copy_object(o) , {depth: null});
+const assert = require('assert').strict;
+assert.ok( [...stringify_g(o_cp)].join('') === JSON.stringify(o_cp),'JSON not equal!' );
+
+
+
+createJSONStream( o, null,3).pipe(process.stdout);
+//you can change object - createJSONStream make a copy to continue processing async.
+Object.keys(o).forEach(k => delete o[k]);
+
 //test huge array
 let very_big_array = [];
-very_big_array[10000000] = o;
+very_big_array[10000000] = 'last';
 
 let out = createWriteStream( __dirname + '/out.json', 'utf8');
-
  console.time('stream');
  createJSONStream( very_big_array, null,2).pipe(out).on('close',()=>{
      console.timeEnd('stream');
-     createJSONStream( o,null,3).pipe(process.stdout);
  });
+ very_big_array.length = 0; //it's ok!
 
 setInterval(()=>process.stdout.write('.'),1000).unref();
--- index.js
+++ index.js
@@ -1,24 +1,9 @@
+/*
+    ver. 2.0.0
+         pass1: object copy with stringify field (JSON.stringify()), circular detect, filter not JSON fields (sync phase)
+         pass2: Create JSON generator. make Readable stream from generator. (async phase)
+*/
 const  { Readable } = require('stream');
-
-function encodeJSONText(s) {
-   let sb = '';
-    for (let i=0; i<s.length; i++) {
-      let cc = s.charCodeAt(i);
-      switch ( cc ) {
-         case 8:  sb += '\\b'; break;
-         case 9:  sb += '\\t'; break;
-         case 10: sb += '\\n'; break;
-         case 12: sb += '\\f'; break;
-         case 13: sb += '\\r'; break;
-         case 34: sb += '\\"'; break;
-         case 92: sb += '\\\\'; break;
-         case 47: sb += '\\/'; break;
-         default:
-            sb += ((cc >= 0x20) && (cc <= 0x7f)) ? s[i] : '\\u'+('000' + cc.toString(16)).slice(-4);
-      }
-   }
-   return sb;
-}
 
 function * blockify_sync( generator, size = 4096 ) {
    let buffer = '';
@@ -38,75 +23,111 @@ function * bufferify_sync( generator ) {
    }
 }
 
-function * stringify_sync( value, circular_protection_set ) {
-   /* type unpacking first */
-   if (value instanceof String)
-      value = String(value);
-   else if (value instanceof Number)
-      value = Number(value);
-   else if (value instanceof Date)
-      value = value.toISOString();
-   else if (value instanceof Boolean)
-      value = Boolean(value);
+function try_stringify( value) {
+   /* type unboxing first */
+   if (typeof value === 'object') {
+
+      if ((value === null) ||
+         ((Array.isArray(value) && (value.length===0))) ||
+         (value instanceof String) ||
+         (value instanceof Number) ||
+         (value instanceof Date)   ||
+         (value instanceof Boolean))
+         return JSON.stringify(value);
+      else
+         return;
+   }
    switch (typeof value) {
    case 'boolean':
-         yield value ? 'true' : 'false';
-         break;
    case 'number':
-         yield String(value);
-         break;
    case 'string':
-         yield `"${encodeJSONText(value)}"`
-         break;
-   case 'object':
-         if (value === null) {
-            yield 'null';
-            break;
-         }
-         /*
-         if (typeof value.toJSON === 'function') {
-            yield value.toJSON()
-            break;
-         }*/
-         if ((circular_protection_set) && (circular_protection_set.has(value))) {
-            yield 'null';
-            break;
-         }
-         if (circular_protection_set)
-            circular_protection_set.add(value);
-         else
-            circular_protection_set = new Set([value]);
-         if (Array.isArray(value)) {
-            yield '[';
-            for (let i=0; i< value.length; i++) {
-               if (i > 0)
-                  yield ',';
-               if (value[i] !== void 0) {
-                  yield * stringify_sync( value[i], circular_protection_set );
-               } else {
-                  yield 'null';
-               }
-            }
-            yield ']';
+         return JSON.stringify(value);
+   }
+}
+
+function copy_object( from, set ) {
+   if (from === void 0) {
+      return;
+   }
+   let to = try_stringify(from);
+   if (typeof to === 'string' ) {
+      return to;
+   }
+   if (typeof from !== 'object')
+      return;
+   if (set) {
+      if (set.has(from)) {
+         return;
+      }
+      set.add(from);
+   }
+   else
+      set = new Set([from]);
+
+   const is_array = Array.isArray(from);
+
+   let result;
+   let empty  = true;
+
+   if (is_array) {
+      result = [];
+      result.length = from.length;
+   } else
+      result = {};
+
+   for (let item of Object.entries(from)) {
+      let value = copy_object( item[1], set );
+      if (value === void 0) {
+         if (is_array)
+            result[ item[0] ] = 'null';
+      } else {
+         result[ item[0] ] = value;
+         empty = false;
+      }
+   }
+   set.delete(from);
+   if ((!is_array) && (empty))
+      return '{}'
+   else
+      return result;
+}
+
+function * stringify_sync( value ) {
+
+   if (value === void 0)
+      return;
+
+   if (typeof value === 'string') {
+      yield value;
+      return;
+   }
+
+   if (Array.isArray(value)) {
+      yield '[';
+      for (let i=0; i< value.length; i++) {
+         if (i > 0)
+            yield ',';
+         if (value[i] !== void 0) {
+            yield * stringify_sync( value[i] );
          } else {
-            yield '{';
-            let count = 0;
-            for (let key in value) {
-               let item = value[key];
-               if (item !== void 0) {
-                  if (count++ > 0)
-                     yield ',';
-                  yield `"${encodeJSONText(key)}"`;
-                  yield ':';
-                  yield * stringify_sync( item, circular_protection_set );
-               }
-            }
-            yield '}';
+            yield 'null';
          }
-         circular_protection_set.delete(value);
-         break;
-   default:
-      yield `"${encodeJSONText(String(value))}"`
+      }
+      yield ']';
+   } else {
+      yield '{';
+      let count = 0;
+      for (let key in value) {
+         let item = value[key];
+         if (item !== void 0) {
+            if (count++ > 0)
+               yield ',';
+            yield JSON.stringify(key);
+            yield ':';
+            yield * stringify_sync( item  );
+         }
+      }
+      yield '}';
    }
 }
 
@@ -142,10 +163,10 @@ function * prettify_sync( value, spaces=0 ) {
 }
 
 //low level function exports...
-exports.stringify_g = stringify_sync;
-exports.prettify_g = prettify_sync;
+exports.stringify_g = (value, spaces) => prettify_sync( copy_object(value),spaces );
 exports.blockify_g = blockify_sync;
 exports.bufferify_g = bufferify_sync;
+exports.copy_object = copy_object;
 
 exports.createJSONStream = (o, ignore, spaces, options, block_size) => {
    if ((typeof options === 'number') &&
@@ -153,6 +174,5 @@ exports.createJSONStream = (o, ignore, spaces, options, block_size) => {
          block_size = options;
          options = void 0;
       }
-
-   return Readable.from( blockify_sync( prettify_sync(o,spaces), block_size ), options );
+   return Readable.from( blockify_sync( prettify_sync( copy_object( o ),spaces), block_size ), options );
 }
